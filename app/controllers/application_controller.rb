0509033c49af76072380a771acc8fb1a27c717fa
sefu9090 <fsaul16@yahoo.com>
2016-03-15 21:49:04 +0000

    pushing

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+require './config/environment'
+require './app/models/tweet'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    @tweets = Tweet.all
+    erb :index 
+    
+  end
+    get '/tweet' do
+    erb :tweet
+  end 
+  post '/tweet' do
+    Tweet.new(params[:username], params[:status])
+    redirect '/'
+    end
+end
